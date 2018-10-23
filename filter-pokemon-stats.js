d0063d7b9a895019fb755b9381c1936110daaa50
Rplus <rplus.tw@gmail.com>
2018-10-23 15:43:52 +0800

    add task: filter-pm-stats
    filter raw data with used type only

---
+++ filter-pokemon-stats.js
@@ -0,0 +1,35 @@
+let fs = require('fs');
+
+let contents = fs.readFileSync('GAME_MASTER.json', 'utf8');
+
+let allData = JSON.parse(contents);
+
+let pmData = allData.itemTemplates.filter(d => d.pokemonSettings);
+
+console.log(`All pm data length: ${pmData.length}`);
+
+let props = [
+  'pokemonId',
+  'type',
+  'type2',
+  'stats',
+  'familyId',
+  'candyToEvolve',
+  'kmBuddyDistance',
+];
+
+if (process.argv[2] === 'filter=true') {
+  pmData = pmData.map(pm => {
+    for (const prop in pm.pokemonSettings) {
+      if (!props.includes(prop)) {
+        delete pm.pokemonSettings[prop];
+      }
+    }
+    pm.pokemonSettings.pokedex = +pm.templateId.match(/V(\d{4})/)[1];
+    return pm;
+  });
+}
+
+let fileContent = JSON.stringify(pmData, null, 2);
+fs.writeFileSync('pm-data.json', fileContent);
+console.log(`JSON saved! ( ${fileContent.length / 1000} kb )`);
--- makefile
+++ makefile
@@ -1,2 +1,7 @@
+filter = true
+
 get-data:
 	wget -q https://raw.githubusercontent.com/pokemongo-dev-contrib/pokemongo-game-master/master/versions/latest/GAME_MASTER.json -O GAME_MASTER.json
+
+filter-pm-stats:
+	node filter-pokemon-stats.js filter=${filter}
